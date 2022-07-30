36f7d65db852d0d655050f79207627a7c9b6a04a
Daniel Montilla <montilla.d2000@gmail.com>
2022-07-30 15:57:45 -0400

    nada

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ def main():
     cap = cv.VideoCapture(0)
     manos = mp.solutions.hands.Hands()
     dibujo = mp.solutions.drawing_utils
+    estilos = mp.solutions.drawing_styles
 
     while True:
         exito, img = cap.read()
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        res = manos.process(imgRGB)
+        resultado = manos.process(imgRGB)
+
+        land_marks = resultado.multi_hand_landmarks
+
+        if land_marks:
+            for land_mark in land_marks:
+                dibujo.draw_land_marks(resultado,
+                                       land_marks,
+                                       manos.HAND_CONNECTIONS,
+                                       estilos.get_default_hand_landmarks_style(),
+                                       estilos.get_default_hand_connections_style())
 
         cv.imshow('video', img)
         cv.waitKey(1)
