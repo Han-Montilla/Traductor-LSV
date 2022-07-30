ee035501e9255cf3cca35941fcd82d47efbde066
Daniel Montilla <montilla.d2000@gmail.com>
2022-07-30 15:34:58 -0400

    camara

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import cv2 as cv
+import mediapipe as mp
+
+
 def main():
-    pass
+    cap = cv.VideoCapture(0)
+    manos = mp.solutions.hands.Hands()
+    dibujo = mp.solutions.drawing_utils
+
+    while True:
+        exito, img = cap.read()
+        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        res = manos.process(imgRGB)
+
+        cv.imshow('video', img)
+        cv.waitKey(1)
+
 
 if __name__ == '__main__':
     main()
