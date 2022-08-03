7ab948326f500c216d3ef18814c400379503ee37
xapho <kevin.han@correo.unimet.edu.ve>
2022-08-03 19:53:15 -0400

    adelantando pequenas cosas

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from turtle import color
+from typing import Any
 import cv2 as cv
+from cv2 import blur
+from cv2 import scaleAdd
+from cv2 import adaptiveThreshold
+from cv2 import CamShift
+from matplotlib.pyplot import gray
 import mediapipe as mp
+from numpy import place
 
 
 def main():
@@ -11,13 +19,13 @@ def main():
     #creamos un objeto que va a recibir la deteccion
     mp_hands = mp.solutions.hands
 
-    #metodo qye crea los puntos criticos calculados matematicamente en las manos
+    #metodo que crea los puntos criticos calculados matematicamente en las manos
     mp_drawing = mp.solutions.drawing_utils
 
     #el estilo que tendran los puntos
     mp_drawing_styles = mp.solutions.drawing_styles
 
-    #Verificamos si la camara funciona y agregamos parametros  
+    #Verificamos si la camara funciona con las manos y agregamos parametros  
     with mp_hands.Hands() as hands:
         while cap.isOpened():
             success, img = cap.read()
@@ -32,8 +40,12 @@ def main():
             img.flags.writeable = True
             img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
             
-            #Si funciona aplicamos parametros y se empieza la camara
-            if results.multi_hand_landmarks:
+
+
+            #Si funciona aplicamos parametros para que se puedan ver en la camara
+            if results.multi_hand_landmarks:               
+                
+                #detecta los puntos en las manos y los dibuja
                 for hand_landmarks in results.multi_hand_landmarks:
                     mp_drawing.draw_landmarks(
                         img,
@@ -41,7 +53,9 @@ def main():
                         mp_hands.HAND_CONNECTIONS,
                         mp_drawing_styles.get_default_hand_landmarks_style(),
                         mp_drawing_styles.get_default_hand_connections_style())
+            
 
+            cv.rectangle(img,(100-2,300-2),(100+3,300+3),(0,255,0),6)
             cv.imshow('video', img)
             cv.waitKey(5)
 
