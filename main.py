40dd4c6e0d4bc495b9f873a860262198375faeac
Daniel Montilla <montilla.d2000@gmail.com>
2022-08-25 22:56:35 -0400

    recoleccion preliminar

--- main.py
+++ main.py
@@ -4,37 +4,38 @@ import numpy as np
 import mediapipe as mp
 import os
 
-def main():
-  mp_holistic = mp.solutions.holistic  # Holistic model
-  mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
-    
-  def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False                   # Image is no longer writeable
-    results = model.process(image)                  # Make prediction
-    image.flags.writeable = True                    # Image is now writeable
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
-    return image, results
+mp_holistic = mp.solutions.holistic  # Holistic model
+mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
 
-  def draw_styled_landmarks(image, results):
-    # Draw left hand
-    mp_drawing.draw_landmarks(
-      image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-      mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
-      mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-    ) 
-    # Draw right hand
-    mp_drawing.draw_landmarks(
-      image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-      mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
-      mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-    )
-  
-  def extract_keypoints(results):
-    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-    return np.concatenate([lh, rh])
-  
+# utilidad
+def mediapipe_detection(image, model):
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
+  image.flags.writeable = False                   # Image is no longer writeable
+  results = model.process(image)                  # Make prediction
+  image.flags.writeable = True                    # Image is now writeable
+  image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
+  return image, results
+
+def draw_styled_landmarks(image, results):
+  # Draw left hand
+  mp_drawing.draw_landmarks(
+    image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+    mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+    mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+  ) 
+  # Draw right hand
+  mp_drawing.draw_landmarks(
+    image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+  )
+
+def extract_keypoints(results):
+  lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+  rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+  return np.concatenate([lh, rh])
+
+def main():
   cap = cv2.VideoCapture(0)
   # Set mediapipe model 
   with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
@@ -46,7 +47,7 @@ def main():
       image, results = mediapipe_detection(frame, holistic)
       
       # Draw landmarks
-      draw_styled_landmarks(image, results)
+      draw_styled_landmarks(mp_drawing, mp_holistic, image, results)
 
       # Show to screen
       cv2.imshow('LSV Traductor', image)
@@ -59,11 +60,12 @@ def main():
 
 # TODO: separar a otro archivo
 def generate_data():
+  
   # Path for exported data, numpy arrays
   DATA_PATH = os.path.join('data') 
 
   # Actions that we try to detect
-  signs = np.array(['a', 'b', 'c', 'd', 'e'])
+  signs = np.array(['a'])
 
   # Thirty videos worth of data
   no_sequences = 30
@@ -77,6 +79,51 @@ def generate_data():
         os.makedirs(os.path.join(DATA_PATH, sign, str(sequence)))
       except:
         pass
+  
+  cap = cv2.VideoCapture(0)
+  # Set mediapipe model 
+  with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    # Loop through actions
+    for sign in signs:
+      # Loop through sequences aka videos
+      for sequence in range(no_sequences):
+        # Loop through video length aka sequence length
+        for frame_num in range(sequence_length):
+          # Read feed
+          ret, frame = cap.read()
+
+          # Make detections
+          image, results = mediapipe_detection(frame, holistic)
+
+          # Draw landmarks
+          draw_styled_landmarks(image, results)
+          
+          # NEW Apply wait logic
+          if frame_num == 0: 
+            cv2.putText(image, 'STARTING COLLECTION', (120,200), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
+            cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(sign, sequence), (15,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+            # Show to screen
+            cv2.imshow('OpenCV Feed', image)
+            cv2.waitKey(2000)
+          else: 
+            cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(sign, sequence), (15,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+            # Show to screen
+            cv2.imshow('OpenCV Feed', image)
+          
+          # NEW Export keypoints
+          keypoints = extract_keypoints(results)
+          npy_path = os.path.join(DATA_PATH, sign, str(sequence), str(frame_num))
+          np.save(npy_path, keypoints)
+
+          # Break gracefully
+          if cv2.waitKey(10) & 0xFF == ord('q'):
+              break
+                    
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
   generate_data()
